83e23f46e9ac7e924ad87f4c64fba955611848d4
Xe <me@christine.website>
2023-01-19 20:29:35 -0500

    initial commit
    Signed-off-by: Xe <me@christine.website>

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ rustc cargo rustfmt rust-analyzer ed ];
+        };
+      });
+}
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![allow(non_snake_case)]
+
+use std::{env, os::unix::process::CommandExt, process::Command};
+use syslog::{unix, Facility::LOG_AUTH, Formatter3164};
+
+fn main() -> anyhow::Result<()> {
+    if env::args().len() == 1 {
+        eprintln!("usage: {} <command> [args]", env::args().nth(0).unwrap());
+        return Ok(());
+    }
+
+    let program = env::args().nth(1).unwrap();
+    let mut writer = unix(Formatter3164 {
+        facility: LOG_AUTH,
+        hostname: None,
+        process: "xn--ts9h".into(),
+        pid: 0,
+    })
+    .unwrap();
+    writer.err(format!("running {program}")).unwrap();
+
+    Err(Command::new(program)
+        .args(env::args().skip(2).collect::<Vec<String>>())
+        .uid(0)
+        .gid(0)
+        .exec()
+        .into())
+}
